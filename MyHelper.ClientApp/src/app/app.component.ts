0170acb43ff4528f632ac3849753d48aa3bd0e5f
pastore <pastorednepr@gmail.com>
2017-12-28 22:06:11 +0200

    started to  add styles

--- MyHelper.ClientApp/src/app/app.component.ts
+++ MyHelper.ClientApp/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mh-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My helper';
 }
