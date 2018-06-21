00046a317a7a7af437bbb7be790b272e455e0947
pastore <pastorednepr@gmail.com>
2018-06-22 01:35:41 +0300

    add new ef core migratons

---
+++ MyHelper.Api/Migrations/20180621221257_updateMhTask.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyHelper.Api.Migrations
+{
+    public partial class updateMhTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MhTasks_Tags_TagId",
+                table: "MhTasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Tags_TagId",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notes_TagId",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MhTasks_TagId",
+                table: "MhTasks");
+
+            migrationBuilder.DropColumn(
+                name: "TagId",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduleMhTaskId",
+                table: "MhTasks");
+
+            migrationBuilder.DropColumn(
+                name: "TagId",
+                table: "MhTasks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "TagId",
+                table: "Notes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ScheduleMhTaskId",
+                table: "MhTasks",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "TagId",
+                table: "MhTasks",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_TagId",
+                table: "Notes",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MhTasks_TagId",
+                table: "MhTasks",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MhTasks_Tags_TagId",
+                table: "MhTasks",
+                column: "TagId",
+                principalTable: "Tags",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Tags_TagId",
+                table: "Notes",
+                column: "TagId",
+                principalTable: "Tags",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
