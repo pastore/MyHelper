6c4d55a8f4f57e4d99f30e0ada3800356cee82fd
pastore <pastorednepr@gmail.com>
2019-02-11 23:36:14 +0200

    fixed lazy loading for aot compile

--- MyHelper.ClientApp/src/app/system/system-routing.module.ts
+++ MyHelper.ClientApp/src/app/system/system-routing.module.ts
@@ -7,15 +7,13 @@ import { NotesPageComponent } from './notes/notes-page.component';
 import { TasksPageComponent } from './tasks/tasks-page.component';
 import { FeedsPageComponent } from './feeds/feeds-page.component';
 
-import { FriendsModule } from './friends/friends.module';
-
 const routes: Routes = [
   { path: '', component: SystemComponent, canActivate: [AuthGuard],
       children: [
         {path: 'feeds', component: FeedsPageComponent},
         {path: 'notes', component: NotesPageComponent},
         {path: 'tasks', component: TasksPageComponent},
-        {path: 'friends', loadChildren: () => FriendsModule}
+        {path: 'friends', loadChildren: './friends/friends.module#FriendsModule'}
       ]
   }
 ];
