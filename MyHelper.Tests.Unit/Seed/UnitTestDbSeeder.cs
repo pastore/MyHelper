891d14cf67ff3675b3ef7244ef9787d4e671c155
Serhii Tsyhanivskyi <Serhii_Tsyhanivskyi@epam.com>
2019-10-08 16:49:10 +0300

    remove transactions for inmemory db

--- MyHelper.Tests.Unit/Seed/UnitTestDbSeeder.cs
+++ MyHelper.Tests.Unit/Seed/UnitTestDbSeeder.cs
@@ -17,94 +17,84 @@ namespace MyHelper.Tests.Unit.Seed
 
         public void SeedDb()
         {
-            using (var transaction = _myHelperDbContext.Database.BeginTransaction())
+
+            if (!_myHelperDbContext.AppUsers.Any(x => x.UserRole == EUserRole.Admin))
             {
-                try
+                var user1 = new AppUser
                 {
-                    if (!_myHelperDbContext.AppUsers.Any(x => x.UserRole == EUserRole.Admin))
-                    {
-                        var user1 = new AppUser
-                        {
-                            Email = "admin1@admin1.com",
-                            Username = "admin1",
-                            Password = HashPasswordHelper.Hash("admin1"),
-                            UserRole = EUserRole.Admin,
-                            CreatedDate = DateTime.Now
-                        };
-                        
-                        var user2 = new AppUser
-                        {
-                            Email = "user2@user2.com",
-                            Username = "user2",
-                            Password = HashPasswordHelper.Hash("user2"),
-                            UserRole = EUserRole.User,
-                            CreatedDate = DateTime.Now
-                        };
+                    Email = "admin1@admin1.com",
+                    Username = "admin1",
+                    Password = HashPasswordHelper.Hash("admin1"),
+                    UserRole = EUserRole.Admin,
+                    CreatedDate = DateTime.Now
+                };
 
-                        var user3 = new AppUser
-                        {
-                            Email = "user3@user3.com",
-                            Username = "user3",
-                            Password = HashPasswordHelper.Hash("user3"),
-                            UserRole = EUserRole.User,
-                            CreatedDate = DateTime.Now
-                        };
-
-                        _myHelperDbContext.AppUsers.AddRange(new List<AppUser>{ user1, user2, user3 });
-                        _myHelperDbContext.SaveChanges();
-                    }
-
-                    if (!_myHelperDbContext.Tags.Any() && !_myHelperDbContext.Notes.Any() && !_myHelperDbContext.NoteTags.Any())
-                    {
-                        var note1 = new Note
-                        {
-                            Name = "note_name1",
-                            Description = "note_name1_description",
-                            CreateDate = DateTime.Now,
-                            UpdateDate = DateTime.Now,
-                            AppUser = _myHelperDbContext.AppUsers.FirstOrDefault()
-                        };
-
-                        var note2 = new Note
-                        {
-                            Name = "note_name2",
-                            Description = "note_name2_description",
-                            CreateDate = DateTime.Now,
-                            UpdateDate = DateTime.Now,
-                            AppUser = _myHelperDbContext.AppUsers.Skip(1).FirstOrDefault()
-                        };
-
-                        var note3 = new Note
-                        {
-                            Name = "note_name3",
-                            Description = "note_name3_description",
-                            CreateDate = DateTime.Now,
-                            UpdateDate = DateTime.Now,
-                            AppUser = _myHelperDbContext.AppUsers.Skip(2).FirstOrDefault()
-                        };
-
-                        var tags = new Tag[]
-                        {
-                            new Tag { Name = "tag1" },
-                            new Tag { Name = "tag2" },
-                            new Tag { Name = "tag3" }
-                        };
-
-                        _myHelperDbContext.AddRange(
-                           new NoteTag { Note = note1, Tag = tags[0] },
-                           new NoteTag { Note = note2, Tag = tags[1] },
-                           new NoteTag { Note = note3, Tag = tags[2] }
-                        );
-
-                        _myHelperDbContext.SaveChanges();
-                    }
-
-                    transaction.Commit();
-                }
-                catch
+                var user2 = new AppUser
                 {
-                    transaction.Rollback();
-                }
+                    Email = "user2@user2.com",
+                    Username = "user2",
+                    Password = HashPasswordHelper.Hash("user2"),
+                    UserRole = EUserRole.User,
+                    CreatedDate = DateTime.Now
+                };
+
+                var user3 = new AppUser
+                {
+                    Email = "user3@user3.com",
+                    Username = "user3",
+                    Password = HashPasswordHelper.Hash("user3"),
+                    UserRole = EUserRole.User,
+                    CreatedDate = DateTime.Now
+                };
+
+                _myHelperDbContext.AppUsers.AddRange(new List<AppUser> { user1, user2, user3 });
+                _myHelperDbContext.SaveChanges();
+            }
+
+            if (!_myHelperDbContext.Tags.Any() && !_myHelperDbContext.Notes.Any() && !_myHelperDbContext.NoteTags.Any())
+            {
+                var note1 = new Note
+                {
+                    Name = "note_name1",
+                    Description = "note_name1_description",
+                    CreateDate = DateTime.Now,
+                    UpdateDate = DateTime.Now,
+                    AppUser = _myHelperDbContext.AppUsers.FirstOrDefault()
+                };
+
+                var note2 = new Note
+                {
+                    Name = "note_name2",
+                    Description = "note_name2_description",
+                    CreateDate = DateTime.Now,
+                    UpdateDate = DateTime.Now,
+                    AppUser = _myHelperDbContext.AppUsers.Skip(1).FirstOrDefault()
+                };
+
+                var note3 = new Note
+                {
+                    Name = "note_name3",
+                    Description = "note_name3_description",
+                    CreateDate = DateTime.Now,
+                    UpdateDate = DateTime.Now,
+                    AppUser = _myHelperDbContext.AppUsers.Skip(2).FirstOrDefault()
+                };
+
+                var tags = new Tag[]
+                {
+                    new Tag { Name = "tag1" },
+                    new Tag { Name = "tag2" },
+                    new Tag { Name = "tag3" }
+                };
+
+                _myHelperDbContext.AddRange(
+
+                   new NoteTag { Note = note1, Tag = tags[0] },
+                   new NoteTag { Note = note2, Tag = tags[1] },
+                   new NoteTag { Note = note3, Tag = tags[2] }
+                );
+
+                _myHelperDbContext.SaveChanges();
             }
         }
     }
--- MyHelper.Tests.Unit/Services/NoteServiceTests.cs
+++ MyHelper.Tests.Unit/Services/NoteServiceTests.cs
@@ -12,21 +12,24 @@ namespace MyHelper.Tests.Unit.Services
     public class NoteServiceTests
     {
         private INoteService _noteService;
+        private MyHelperContext _myHelperDbContext;
 
         [SetUp]
         public void SetUp()
         {
             var dbOptions = new DbContextOptionsBuilder<MyHelperContext>().UseInMemoryDatabase(databaseName: "TestDb").Options;
-            var myHelperDbContextt = new MyHelperContext(dbOptions);
-            new UnitTestDbSeeder(myHelperDbContextt).SeedDb();
-            _noteService = new NoteService(myHelperDbContextt, new Mock<IMapper>().Object);
+            _myHelperDbContext = new MyHelperContext(dbOptions);
+            new UnitTestDbSeeder(_myHelperDbContext).SeedDb();
+            _noteService = new NoteService(_myHelperDbContext, new Mock<IMapper>().Object);
         }
 
         [Test]
         public void GetNotesAsyncTest()
         {
-
+            var users = _myHelperDbContext.AppUsers;
+            var notes = _myHelperDbContext.Notes;
+            var noteTags = _myHelperDbContext.NoteTags;
+            var tags = _myHelperDbContext.Tags;
         }
-
     }
 }
